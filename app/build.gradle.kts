adbfcac5e9cd880b912ee20efc66fe382af140d7
RA415428 <nayakhardayal4@gmail.com>
2026-09-09 04:51:08 +0530

    Fix Kotlin JVM target configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,6 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlinOptions {
-    jvmTarget = "17"
-  }
   buildFeatures {
     compose = true
     buildConfig = true
